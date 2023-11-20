1456e73d82420b60c7df6c196ccd85722d58aa89
hdh12191 <hdh12191@naver.com>
2023-11-20 21:04:02 +0900

    TEXTRPG

---
+++ TEXTRPG2/Monster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEXTRPG2
+{
+    enum MonsterType
+    {
+        None = 0,
+        Slime = 1,
+        Orc = 2,
+        Skeleton = 3
+    }
+    class Monster : Creature
+    {
+        protected MonsterType _type = MonsterType.None;
+
+        protected Monster(MonsterType type) : base(CreatureType.Monster)
+        {
+            _type = type;
+        }
+        public MonsterType GetMonsterType() { return _type; }
+    }
+
+    class Slime : Monster
+    {
+        public Slime() : base(MonsterType.Slime)
+        {
+            SetInfo(10, 3);
+        }
+    }
+
+    class Orc : Monster
+    {
+        public Orc() : base(MonsterType.Orc)
+        {
+            SetInfo(20, 7);
+        }
+    }
+    class Skeleton : Monster
+    {
+        public Skeleton() : base(MonsterType.Skeleton)
+        {
+            SetInfo(15, 5);
+        }
+    }
+}
---
+++ TEXTRPG2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TEXTRPG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            while(true)
+            {
+                game.Process();
+            }
+        }
+    }
+}
